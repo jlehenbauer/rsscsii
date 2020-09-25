0351c03644c086e0f08f776877a328a33abf4dcf
Jacob Lehenbauer <jake@lehenbauer.us>
2020-09-25 10:04:59 -0500

    Corrected comments on 1-CollectNewspaperKarel

--- karel-project/1-CollectNewspaperKarel.py
+++ karel-project/1-CollectNewspaperKarel.py
@@ -10,6 +10,26 @@ newspaper (represented by a beeper, of course), and then return
 to its initial position in the upper left corner of the house.
 """
 
+"""
+In all Karel projects, Karel only begins with the following
+abilities:
+
+move()
+ - Karel moves one space in whichever direction they 
+   are facing
+
+turn_left()
+ - Karel turns left once (90 degrees)
+
+pick_beeper()
+ - Karel picks up a beeper on Karel's current spot
+
+put_beeper()
+ - Karel places a beeper on Karel's current spot
+
+See karel-reference.md for all Karel's capabilities.
+"""
+
 
 def main():
 	pass
