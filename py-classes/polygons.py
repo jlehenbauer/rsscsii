7bafeec8f9fc1623f573a180b4c70b74e407c4dc
Kate Nemec <krnemec@gmail.com>
2021-03-03 14:36:29 -0600

    added attributes and constructer to the polygon class

--- py-classes/polygons.py
+++ py-classes/polygons.py
@@ -1,2 +1,11 @@
 class Polygon:
     sides = None
+    radius = None
+    side_length = None
+    height = None
+    width = None
+    perimiter = None
+    area = None
+    def __init__(self):
+        self.sides = 3
+        self.side_length = 1
