dd46386066b3cc1e34c2e9bea5739cc4ce8e3972
Kate Nemec <krnemec@gmail.com>
2021-03-10 14:44:52 -0600

    added input yay

--- py-classes/polytest.py
+++ py-classes/polytest.py
@@ -3,5 +3,6 @@ from polygons import Polygon
 def main():
     poly=Polygon(4, 3)
     print(poly.get_area())
+    sides = int(input("how many sides do you want?"))
 if __name__ == "__main__":
     main()
