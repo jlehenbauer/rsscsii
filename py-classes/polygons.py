032754a57889ea44af621b374b10f9c7fc864095
Kate Nemec <krnemec@gmail.com>
2021-03-10 14:15:52 -0600

    finishing up get area function 5.0

--- py-classes/polygons.py
+++ py-classes/polygons.py
@@ -3,16 +3,16 @@ import math
 class Polygon:
     
     radius = None
-    height = Nonecs
+    height = None
     width = None
     
     def __init__(self, sides=3, side_length=1):
         self.sides = sides
         self.side_length = side_length
         self.perimeter = self.sides * self.side_length
-        self.apothem = apothem = self.side_length / (2 * math.tan(math.pi/self.sides))
+        self.apothem = self.side_length / (2 * math.tan(math.pi/self.sides))
         self.area = 1/2*(self.apothem * self.perimeter)
-    #this function uses self to find perimeter of polygon
+    #this function uses self to find the area of our polygon
     def get_area(self):
         return self.area
 
