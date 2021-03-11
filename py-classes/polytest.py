634422609772f11c4a30554d3a023e1183ac6028
Kate Nemec <krnemec@gmail.com>
2021-03-11 14:15:05 -0600

    The user is now able to make their own shape and they the area, perimeter and apothem back.

--- py-classes/polytest.py
+++ py-classes/polytest.py
@@ -1,8 +1,13 @@
 from polygons import Polygon
 
 def main():
-    poly=Polygon(4, 3)
+    sides = int(input("how many sides do you want? "))
+    side_length = int(input("what do you want the length of the sides to be? "))
+    poly=Polygon(sides, side_length)
     print(poly.get_area())
-    sides = int(input("how many sides do you want?"))
+
+    print("Your polygon's apothem is " + str(poly.apothem))
+    print("Your polygon's perimeter is " + str(poly.perimeter))
+    print("Your polygon's area is " + str(poly.area))
 if __name__ == "__main__":
     main()
