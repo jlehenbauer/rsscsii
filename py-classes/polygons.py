9f8e45c3adbde41ca4e64809c961711f25a60656
Kate Nemec <krnemec@gmail.com>
2021-03-12 14:11:44 -0600

    Classes have been added for hexagon, heptagon, octogon and have variables based on their formulas

--- py-classes/polygons.py
+++ py-classes/polygons.py
@@ -21,4 +21,24 @@ class Triangle(Polygon):
         super().__init__(3, side_length)
         self.height = self.apothem * 3
         self.width = self.side_length
-
+class Hexagon(Polygon):
+    def __init__(self, side_length=1):
+        super().__init__(6, side_length)
+        self.height = self.apothem * 3
+        self.width = self.side_length
+        self.long_diagonal = self.side_length * 2
+        self.short_diagonal = math.root(3) * self.side_length
+class Heptagon(Polygon):
+    def __init__(self, side_length=1):
+        super().__init__(7, side_length)
+        self.height = self.side_length/(2 * math.tan((math.pi/2)/self.sides))
+        self.width = self.side_length
+        self.long_diagonal = self.side_length / (2 * math.sin((math.pi/2)/self.sides))
+        self.short_diagonal = 2 * self.side_length * math.cos(math.pi/self.sides)
+class Octagon(Polygon):
+    def __init__(self, side_length=1):
+        super().__init__(8, side_length)
+        self.height = self.apothem * 3
+        self.width = self.side_length
+        self.long_diagonal = self.side_length * math.root(4 + 2 * math.root(2)) 
+        self.short_diagonal = math.root(3) * self.side_length
