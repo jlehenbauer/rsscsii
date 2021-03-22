289c59481fd657744ea4312eaac8514cd7e51527
Kate Nemec <krnemec@gmail.com>
2021-03-22 14:44:51 -0500

    started choosing item in lists

--- py-classes/Clue/clue.py
+++ py-classes/Clue/clue.py
@@ -1,20 +1,39 @@
 class Clue:
     myList = None
+    weaponsList = ["Candle Stick", "Knife", "Lead Pipe", "Revolver", "Rope", "Wrench"]
+    murderersList = ["Colonel Mustard", "Mrs. White", "Professor Plum", "Mrs. Peacock", "Mr. Green", "Miss Scarlet"]
+    roomsList = ["Ballroom", "Billiard Room", "Conservatory", "Dining Room", 'Hall', "Kitchen", "Lounge", "Library", "Study"]
 
+    
     def __init__(self):
         self.listLen = len(myList)
-        myChoice = 
+        self.weapons = []
+        for item in weaponsList:
+            self.weapons.append(Weapon(item))
+
+        self.murderer = []
+        for item in murderersList:
+            self.murderer.append(Murderer(item))
+
+        self.rooms = []
+        for item in roomsList:
+            self.rooms.append(Room(item))
+
+class Weapon:
+    name = None
+    def __init__(self, name):
+        self.name = name
+
+class Murderer:
+    name = None
+    def __init__(self, name):
+        self.name = name
 
 
-class Weapons:
-    weaponsList = [candleStick, knife, leadPipe, revolver, rope, wrench]
-class Murderers:
-    murderersList = [colonelMustard, mrsWhite, professorPlum, mrsPeacock, mrGreen, missScarlet, ]
-class Rooms:
-    roomsList = [ballRoom, billiardRoom, conservatory, diningRoom, hall, kitchen, lounge, library, study]
-
-def init
-
+class Room:
+    name = None
+    def __init__(self, name):
+        self.name = name
 
 
 
