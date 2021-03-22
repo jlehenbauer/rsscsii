c0c94a978220aad6332d14d53062b67c56dd65a2
Kate Nemec <krnemec@gmail.com>
2021-03-22 14:10:54 -0500

    made clue.py file in rsscsii

---
+++ py-classes/Clue/clue.py
@@ -0,0 +1,22 @@
+class Clue:
+    myList = None
+
+    def __init__(self):
+        self.listLen = len(myList)
+        myChoice = 
+
+
+class Weapons:
+    weaponsList = [candleStick, knife, leadPipe, revolver, rope, wrench]
+class Murderers:
+    murderersList = [colonelMustard, mrsWhite, professorPlum, mrsPeacock, mrGreen, missScarlet, ]
+class Rooms:
+    roomsList = [ballRoom, billiardRoom, conservatory, diningRoom, hall, kitchen, lounge, library, study]
+
+def init
+
+
+
+
+
+
