0f558a7054ef4fd3ec33c43514c2d31d502451ef
Kate Nemec <krnemec@gmail.com>
2021-03-23 14:44:50 -0500

    chose ransom from each list

--- py-classes/Clue/clue.py
+++ py-classes/Clue/clue.py
@@ -1,3 +1,5 @@
+import random
+
 class Clue:
     weaponsList = ["Candle Stick", "Knife", "Lead Pipe", "Revolver", "Rope", "Wrench"]
     murderersList = ["Colonel Mustard", "Mrs. White", "Professor Plum", "Mrs. Peacock", "Mr. Green", "Miss Scarlet"]
@@ -5,22 +7,22 @@ class Clue:
 
     
     def __init__(self):
-        self.solution = []
+        
         self.weapons = []
 
         for item in self.weaponsList:
             self.weapons.append(Weapon(item))
-            self.solution.append(Weapon(item))
 
         self.murderer = []
         for item in self.murderersList:
             self.murderer.append(Murderer(item))
-            self.solution.append(Murderer(item))
 
         self.rooms = []
         for item in self.roomsList:
             self.rooms.append(Room(item))
-            self.solution.append(Room(item))
+
+        self.solution = [random.choice(self.weapons), random.choice(self.murderer), random.choice(self.rooms)]
+
 
         
 class Weapon:
