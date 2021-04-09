77fb97ac89d71c7be3cd73d4eec05635a707bbc8
Kate Nemec <krnemec@gmail.com>
2021-04-09 14:41:03 -0500

    made sorter

---
+++ efficiency/sorting_gifs/sort.py
@@ -0,0 +1,11 @@
+def insertion_sort(in_list):
+    new_list = [in_list[0]]
+    for i in in_list[1:]:
+        for j in range(len(new_list)):
+            if i < new_list[j]:
+               new_list = new_list[:j-1] + [i] + new_list[j:]
+        break
+    return [new_list]
+
+if __name__ == "__main__":
+    print(insertion_sort([2, 6, 1, 3, 8, 5, 4, 7]))
