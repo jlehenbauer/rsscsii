42932cdb14c930a0131d55a9fb5aa843180dc78e
Kate Nemec <krnemec@gmail.com>
2021-04-13 14:32:56 -0500

    i need to save lolol

---
+++ efficiency/sorting_gifs/selection_sorted.py
@@ -0,0 +1,12 @@
+
+def check_min(list):
+    min = list[0]
+    for x in list[1:]:
+        if x < min:
+            x = min
+def selection_sort():
+    unsorted = [3, 8, 2, 5, 7, 1, 4, 6]
+    sorted = []
+    check_min()
+    
+
--- py-classes/Clue/main.py
+++ py-classes/Clue/main.py
@@ -15,7 +15,8 @@ def main():
             print(f"The player is {i.name} and their dice number is {game.roll_dice()}")
             for card in i.card_list:
                 print(card.name)
-        input()
+            input()
+
 if __name__ == "__main__":
     main()
 
