3506fdfc37f3ae51cf59d08ce10f929f21a90a90
Kate Nemec <krnemec@gmail.com>
2021-04-21 13:17:24 -0500

    began tkinter

--- gui/tkintertest.py
+++ gui/tkintertest.py
@@ -2,5 +2,11 @@ import tkinter
 
 def main():
 
+    window = tkinter.Tk()
+    greeting = tkinter.Label(text= "Hello, World!")
+    greeting.pack()
+    button = tkinter.Button(text = "Click Me!", height = 5, width = 5, bg = "red", fg = "yellow")
+    button.pack()
+    window.mainloop()
 if __name__ == "__main__":
     main()
