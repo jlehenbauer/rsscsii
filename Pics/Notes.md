ba4e5ae66ecf0f72ad08d4bbe750abc56433b33c
Jacob Lehenbauer <jake@lehenbauer.us>
2021-05-04 09:16:43 -0500

    Added resources for forest_fire assignment and instructions to Pics/Notes.md

--- Pics/Notes.md
+++ Pics/Notes.md
@@ -4,3 +4,7 @@
 Try to create a new image that is made up of tiles of an existing image. 
 
 For example, create one image that has three copies of the `grumpycat.png` image side-by-side.
+
+
+##### _5/4/21_
+Read through the `forest_fire.py` file and complete the `find_flames` function using the instructions in the TODO comments.
---
+++ Pics/forest_fire.py
@@ -0,0 +1,60 @@
+"""
+File: forestfire.py
+----------------
+This program highlights fires in an image by identifying
+pixels who red intensity is more than INTENSITY_THRESHOLD times
+the average of the red, green, and blue values at a pixel.
+Those "sufficiently red" pixels are then highlighted in the
+image and the rest of the image is turned grey, by setting the
+pixels red, green, and blue values all to be the same average
+value.
+"""
+
+
+# The line below imports SimpleImage for use here
+# Its depends on the Pillow package being installed
+from simpleimage import SimpleImage
+
+DEFAULT_FILE = 'greenland-fire.png'
+INTENSITY_THRESHOLD = 1.15
+
+def find_flames(filename):
+    """
+    This function should highlight the "sufficiently red" pixels
+    in the image and grayscale all other pixels in the image
+    in order to highlight areas of wildfires.
+    """
+    image = SimpleImage(filename)
+
+    for px in image:
+        pass
+        #TODO: Calculate average of green, blue, and red and mutiply by INTENSITY_THRESHOLD
+
+        #TODO: If a pixel's red is higher than this average, set it completely red, otherwise, set it to the average
+    
+    return image
+
+def main():
+    # Get file and load image
+    filename = get_file()
+    image = SimpleImage(filename)
+
+    # Show the original fire
+    original_fire = SimpleImage(filename)
+    original_fire.show()
+
+    # Show the highlighted fire
+    highlighted_fire = find_flames(filename)
+    highlighted_fire.show()
+
+    
+def get_file():
+    # Read image file path from user, or use the default file
+    filename = input('Enter image file (or press enter): ')
+    if filename == '':
+        filename = DEFAULT_FILE
+    return filename
+
+
+if __name__ == '__main__':
+    main()
